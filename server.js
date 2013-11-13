58c24ff58395c543fc91806a6eb9aa3ae0d5d4fe
z281625810 <sunfeng16@yahoo.com>
2013-11-13 16:46:33 -0500

    d

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello world');
+   res.json({
+"name" : "Feng Sun",
+"vacation" : {
+	"places": ["44.73,-72.72","44.248, -74.641","43.734, -75.333"]
+	}
+
+});
+
 });
 
 var port = process.env.PORT || 5000;
